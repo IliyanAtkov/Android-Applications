434c90701877f13f5623609ebf291b9918bcc740
PackMan0 <nobody.petkov@gmail.com>
2016-01-08 18:55:12 +0200

    some changes

--- MyLibrary/app/src/main/java/com/example/packman/mylibrary/FavouriteBooksFragment.java
+++ MyLibrary/app/src/main/java/com/example/packman/mylibrary/FavouriteBooksFragment.java
@@ -6,9 +6,11 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 import android.widget.TextView;
 
 public class FavouriteBooksFragment extends Fragment {
+    private ListView listOfBooks;
     public FavouriteBooksFragment() {
     }
 
@@ -16,6 +18,8 @@ public class FavouriteBooksFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        listOfBooks = (ListView) getView().findViewById(R.id.listView);
         return inflater.inflate(R.layout.fragment_favourite_books, container, false);
     }
 }
--- MyLibrary/app/src/main/java/com/example/packman/mylibrary/MainActivity.java
+++ MyLibrary/app/src/main/java/com/example/packman/mylibrary/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -14,7 +15,7 @@ import android.view.MenuItem;
 import com.parse.Parse;
 import com.parse.ParseObject;
 
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends AppCompatActivity {
     ViewPager mViewPager;
     MyLibraryPageAdapter pageAdapter;
     Toolbar toolbar;
